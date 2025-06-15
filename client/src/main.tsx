34eb198c7969680db8eee86ddd8c650a40b95468
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 00:56:58 +0530

    feat: add Toaster component for notifications and update main entry point

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
+import { Toaster } from "@/components/ui/sonner";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={routes} />
+    <Toaster position="top-center" duration={5000} closeButton={true} />
   </StrictMode>
 );
