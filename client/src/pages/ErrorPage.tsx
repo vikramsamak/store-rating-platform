5be0c08b9f1853f82402da1d5020f7b53ad35c70
vikramsamak <vikramsamak02@gmail.com>
2025-06-15 22:00:52 +0530

    feat: add Alert component and integrate it into ErrorPage; update routes to include Profile and handle 404

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,3 +1,13 @@
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
+
 export const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-muted p-4">
+      <Alert className="max-w-md w-full shadow-lg" variant="destructive">
+        <Terminal className="h-5 w-5" />
+        <AlertTitle className="text-lg">Something went wrong</AlertTitle>
+      </Alert>
+    </div>
+  );
 };
--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,6 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import { DashboardLayout } from "./layouts";
-import { ErrorPage, LandingPage, SigninPage, SignupPage } from "./pages";
+import {
+  ErrorPage,
+  LandingPage,
+  Profile,
+  SigninPage,
+  SignupPage,
+} from "./pages";
 
 //Routes
 export const routes = createBrowserRouter([
@@ -30,6 +36,16 @@ export const routes = createBrowserRouter([
       {
         index: true,
         element: <div>Dashboard Home</div>,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "profle",
+        element: <Profile />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "*",
+        element: <div>Not found page</div>,
         errorElement: <ErrorPage />,
       },
     ],
