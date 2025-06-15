076d8828146e28e24545c37d30357c04aed443d5
vikramsamak <vikramsamak02@gmail.com>
2025-06-15 11:28:05 +0530

    feat: implement user authentication with signup, signin, and signout functionality

---
+++ server/src/constants/index.ts
@@ -0,0 +1,5 @@
+export const JWT_MAX_AGE = 15 * 24 * 60 * 60 * 1000;
+
+export const JWT_TOKEN = "JWT_TOKEN";
+
+export const JWT_EXPIRY = "15d";
--- server/src/controllers/authControllers.ts
+++ server/src/controllers/authControllers.ts
@@ -1,7 +1,116 @@
 import { Request, Response } from "express";
+import { prisma } from "../lib";
+import { compare, genSaltSync, hash } from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_EXPIRY, JWT_MAX_AGE, JWT_TOKEN } from "../constants";
+import { envConfig } from "../config/envConfig";
 
-export async function signUp(req: Request, res: Response): Promise<any> {}
+export async function signUp(req: Request, res: Response): Promise<void> {
+  try {
+    const { name, email, address, password, confirmPassword } = req.body;
 
-export async function signIn(req: Request, res: Response): Promise<any> {}
+    const existingUser = await prisma.user.findUnique({ where: { email } });
 
-export async function signOut(req: Request, res: Response): Promise<any> {}
+    if (existingUser) {
+      res.status(409).json({ success: false, error: "User already exists." });
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      res
+        .status(400)
+        .json({ success: false, error: "Passwords do not match." });
+      return;
+    }
+
+    const salt = genSaltSync(10);
+    const hashedPassword = await hash(password, salt);
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        address,
+        password: hashedPassword,
+      },
+    });
+
+    res
+      .status(201)
+      .json({ success: true, message: "User registered successfully." });
+  } catch (error) {
+    console.error("Error during signup:", error);
+    res.status(500).json({ error: "Internal server error." });
+  }
+}
+
+export async function signIn(req: Request, res: Response): Promise<void> {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        address: true,
+        role: true,
+        password: true,
+      },
+    });
+
+    if (!user) {
+      res.status(401).json({ success: false, error: "Invalid credentials." });
+      return;
+    }
+
+    const isPasswordCorrect = await compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      res.status(401).json({ success: false, error: "Invalid credentials." });
+      return;
+    }
+
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      envConfig.JWT_SECRET_KEY,
+      { expiresIn: JWT_EXPIRY }
+    );
+
+    res.cookie(JWT_TOKEN, token, {
+      maxAge: JWT_MAX_AGE,
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
+    const userData = {
+      id: user.id,
+      name: user.name,
+      address: user.address,
+      email: user.email,
+      role: user.role,
+    };
+
+    res.status(200).json({
+      success: true,
+      message: "User successfully signed in.",
+      data: userData,
+    });
+  } catch (error) {
+    console.error("Error during sign in:", error);
+    res.status(500).json({ success: false, error: "Internal server error." });
+  }
+}
+
+export async function signOut(req: Request, res: Response): Promise<any> {
+  try {
+    res.clearCookie(JWT_TOKEN);
+
+    res.json({ success: true, message: "User signed out successfully" });
+  } catch (error) {
+    console.error("Error while signout", error);
+    res.status(500).json({ success: false, error: "Internal server error" });
+  }
+}
