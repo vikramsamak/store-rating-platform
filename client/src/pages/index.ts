4cb4974ed36b0c16dc3a6b1a6741d8d28a48ca93
vikramsamak <vikramsamak02@gmail.com>
2025-06-15 22:01:10 +0530

    feat: add Profile component and export it in the pages index

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,3 +2,4 @@ export { ErrorPage } from "./ErrorPage";
 export { LandingPage } from "./Landing";
 export { SigninPage } from "./Signin";
 export { SignupPage } from "./Signup";
+export { Profile } from "./Profile";
