3d898246ba794095f5436de1e9863ccc22d69bf7
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 00:29:43 +0530

    feat: add GenericCard component for reusable card UI

---
+++ client/src/components/Generics/GenericCard.tsx
@@ -0,0 +1,42 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+interface GenericCardProps {
+  cardTitle: string;
+  cardDescription?: string;
+  cardAction?: React.ReactNode;
+  cardContent: React.ReactNode;
+  cardFooter?: React.ReactNode;
+}
+
+export const GenericCard: React.FC<GenericCardProps> = ({
+  cardTitle,
+  cardDescription,
+  cardAction,
+  cardContent,
+  cardFooter,
+}) => {
+  return (
+    <Card className={cn("min-w-xs sm:min-w-sm md:min-w-md")}>
+      <CardHeader>
+        <CardTitle>{cardTitle}</CardTitle>
+        {cardDescription && <CardDescription>Card Description</CardDescription>}
+        {cardAction && <CardAction>Card Action</CardAction>}
+      </CardHeader>
+      <CardContent>{cardContent}</CardContent>
+      {cardFooter && (
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      )}
+    </Card>
+  );
+};
