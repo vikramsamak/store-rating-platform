0dc8449c55599c909bb72c27b8060453932f750d
vikramsamak <vikramsamak02@gmail.com>
2025-06-15 08:38:38 +0530

    feat: add @types/cors for TypeScript support and update middleware configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, urlencoded, json } from "express";
 import { config } from "dotenv";
+import { authRouter, ratingRouter, storeRouter, userRouter } from "./routes";
+import cors from "cors";
 
 // Load environment variables from .env file
 config();
@@ -8,9 +10,31 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+//Middelwares
+app.use(json());
+
+app.use(urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+  res.send("Welcome To Store Rating Platform.");
 });
+
+//Routes
+app.use("/api/auth", authRouter);
+
+app.use("/api/user", userRouter);
+
+app.use("/api/store", storeRouter);
+
+app.use("/api/rating", ratingRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
