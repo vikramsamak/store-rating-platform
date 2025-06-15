fea10e747fcffadf56896228e9cb5c6103b21ec1
vikramsamak <vikramsamak02@gmail.com>
2025-06-15 22:20:03 +0530

    feat: make pageTitle optional in DashBoardPageWrapper and conditionally render it

--- client/src/components/Dashboard/DashBoardPageWrapper.tsx
+++ client/src/components/Dashboard/DashBoardPageWrapper.tsx
@@ -1,18 +1,30 @@
+import { cn } from "@/lib/utils";
+
 interface DashBoardPageWrapperProps {
-  pageTitle: string;
+  pageTitle?: string;
   children: React.ReactNode;
+  className?: string;
 }
 
 export const DashBoardPageWrapper: React.FC<DashBoardPageWrapperProps> = ({
   pageTitle,
   children,
+  className,
 }) => {
   return (
     <div className="flex h-full flex-col gap-4">
-      <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">{pageTitle}</h1>
-      </div>
-      <div className="flex-1 min-h-0 rounded-lg border border-dashed shadow-sm">
+      {pageTitle && (
+        <div className="flex items-center">
+          <h1 className="text-lg font-semibold md:text-2xl">{pageTitle}</h1>
+        </div>
+      )}
+
+      <div
+        className={cn(
+          "flex-1 min-h-0 rounded-lg border border-dashed shadow-sm",
+          className
+        )}
+      >
         {children}
       </div>
     </div>
