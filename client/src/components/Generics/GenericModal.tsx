4eb52f127e08fdbdff2e9ddda51a1c9c3dfc415c
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 11:52:52 +0530

    feat: add GenericModal component for reusable modal functionality

---
+++ client/src/components/Generics/GenericModal.tsx
@@ -0,0 +1,37 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
+
+interface GenericModalProps {
+  isModalOpen: boolean;
+  onOpenChange: (isOpen: boolean) => void;
+  modalTitle: string;
+  modalDesc: string;
+  modalContent: React.ReactNode;
+  className?: string;
+}
+
+export const GenericModal: React.FC<GenericModalProps> = ({
+  isModalOpen,
+  onOpenChange,
+  modalContent,
+  modalDesc,
+  modalTitle,
+}) => {
+  return (
+    <Dialog open={isModalOpen} onOpenChange={(open) => onOpenChange(open)}>
+      <DialogContent className={cn("min-w-[50vw] min-h-[50vh] overflow-auto")}>
+        <DialogHeader>
+          <DialogTitle>{modalTitle}</DialogTitle>
+          <DialogDescription>{modalDesc}</DialogDescription>
+        </DialogHeader>
+        {modalContent}
+      </DialogContent>
+    </Dialog>
+  );
+};
