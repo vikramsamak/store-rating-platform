83b0505f0ccd57e0f3819c384ff4f6eb40f4cc6b
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 07:28:47 +0530

    feat: update DasboardHeader to display current page name dynamically

--- client/src/components/Dashboard/DasboardHeader.tsx
+++ client/src/components/Dashboard/DasboardHeader.tsx
@@ -1,7 +1,10 @@
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "../ui/sidebar";
+import { useLocation } from "react-router-dom";
 
 export const DasboardHeader = () => {
+  const currentPage = useLocation();
+
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 sticky top-0 z-10">
       <SidebarTrigger className="-ml-1" />
@@ -9,7 +12,7 @@ export const DasboardHeader = () => {
         orientation="vertical"
         className="mr-2 data-[orientation=vertical]:h-4"
       />
-      <p>Current Page</p>
+      <p className="capitalize">{currentPage.pathname.replace("/", "")}</p>
     </header>
   );
 };
