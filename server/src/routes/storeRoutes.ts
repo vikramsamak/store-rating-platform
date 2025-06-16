bef20e8a227afa5ff7ec90f603f2903c49ef7530
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 20:36:46 +0530

    fix: enforce access control for store routes

--- server/src/routes/storeRoutes.ts
+++ server/src/routes/storeRoutes.ts
@@ -18,31 +18,53 @@ import {
   updateStoreValidation,
 } from "../validations";
 import { getStoreByIdValidation } from "../validations/storeValidation";
+import { Role } from "../generated/prisma";
 
 export const storeRouter = Router();
 
 storeRouter.use(authMiddleware);
 
-storeRouter.use(accessMiddleware);
-
-storeRouter.get("/", validationMiddleware(getStoreValidation), getStores);
+storeRouter.get(
+  "/",
+  accessMiddleware([
+    Role.SYSTEM_ADMIN,
+    Role.ADMIN,
+    Role.USER,
+    Role.STORE_OWNER,
+  ]),
+  validationMiddleware(getStoreValidation),
+  getStores
+);
 
 storeRouter.get(
   "/by-id/:id",
+  accessMiddleware([
+    Role.SYSTEM_ADMIN,
+    Role.ADMIN,
+    Role.USER,
+    Role.STORE_OWNER,
+  ]),
   validationMiddleware(getStoreByIdValidation),
   getStoreById
 );
 
-storeRouter.post("/", validationMiddleware(createStoreValidation), createStore);
+storeRouter.post(
+  "/",
+  accessMiddleware([Role.SYSTEM_ADMIN]),
+  validationMiddleware(createStoreValidation),
+  createStore
+);
 
 storeRouter.patch(
   "/:id",
+  accessMiddleware([Role.SYSTEM_ADMIN, Role.STORE_OWNER]),
   validationMiddleware(updateStoreValidation),
   updateStore
 );
 
 storeRouter.delete(
   "/:id",
+  accessMiddleware([Role.SYSTEM_ADMIN]),
   validationMiddleware(deleteStoreValidation),
   deleteStore
 );
