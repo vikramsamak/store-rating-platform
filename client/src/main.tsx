770898425e05d9fdcb1199e5f39065d1b36ab828
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 08:25:42 +0530

    feat: wrap application in AuthProvider for authentication context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
 import { Toaster } from "@/components/ui/sonner";
+import { AuthProvider } from "./contexts/AuthContext";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={routes} />
-    <Toaster position="top-center" duration={5000} closeButton={true} />
+    <AuthProvider>
+      <RouterProvider router={routes} />
+      <Toaster position="top-center" duration={5000} closeButton={true} />
+    </AuthProvider>
   </StrictMode>
 );
