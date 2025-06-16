563be253488c0b63105b7ac6ae9bea6496cde782
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 20:38:03 +0530

    fix: implement access control for system admin overview route

--- server/src/routes/systemAdminRoutes.ts
+++ server/src/routes/systemAdminRoutes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { accessMiddleware, authMiddleware } from "../middelwares";
 import { getSystemAdminOverview } from "../controllers/systemAdminControllers";
+import { Role } from "../generated/prisma";
 
 export const systemAdminRouter = Router();
 
 systemAdminRouter.use(authMiddleware);
 
-systemAdminRouter.use(accessMiddleware);
-
-systemAdminRouter.get("/overview", getSystemAdminOverview);
+systemAdminRouter.get(
+  "/overview",
+  accessMiddleware([Role.SYSTEM_ADMIN]),
+  getSystemAdminOverview
+);
