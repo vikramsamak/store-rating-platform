977c8e44dcf9d93657dfedea676acc86c6df8fc3
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 20:29:06 +0530

    fix: enforce access control for rating routes

--- server/src/routes/ratingRoutes.ts
+++ server/src/routes/ratingRoutes.ts
@@ -18,35 +18,43 @@ import {
   updateRatingValidation,
 } from "../validations";
 import { getRatingByIdValidation } from "../validations/ratingValidations";
+import { Role } from "../generated/prisma";
 
 export const ratingRouter = Router();
 
 ratingRouter.use(authMiddleware);
 
-ratingRouter.use(accessMiddleware);
-
-ratingRouter.get("/", validationMiddleware(getRatingValidation), getRatings);
+ratingRouter.get(
+  "/",
+  accessMiddleware([Role.SYSTEM_ADMIN, Role.STORE_OWNER, Role.USER]),
+  validationMiddleware(getRatingValidation),
+  getRatings
+);
 
 ratingRouter.get(
   "/by-id/:id",
+  accessMiddleware([Role.SYSTEM_ADMIN, Role.STORE_OWNER]),
   validationMiddleware(getRatingByIdValidation),
   getRatingById
 );
 
 ratingRouter.post(
   "/",
+  accessMiddleware([Role.USER]),
   validationMiddleware(createRatingValidation),
   createRating
 );
 
 ratingRouter.patch(
   "/:id",
+  accessMiddleware([Role.USER]),
   validationMiddleware(updateRatingValidation),
   updateRating
 );
 
 ratingRouter.delete(
   "/:id",
+  accessMiddleware([Role.USER]),
   validationMiddleware(deleteRatingValidation),
   deleteRating
 );
