2f4835585b757082c4dce6f8c3df7644ea345e9b
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 07:22:42 +0530

    feat: update AuthService methods to return SuccessApiResponse type

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -1,12 +1,13 @@
 import { apiRequest } from "@/utils";
 import { ApiService } from "./apiService";
+import type { SuccessApiResponse } from "@/types";
 
 export class AuthService extends ApiService {
   constructor(endpoint: string) {
     super(endpoint);
   }
 
-  async signIn(data: Record<string, unknown>): Promise<unknown> {
+  async signIn(data: Record<string, unknown>): Promise<SuccessApiResponse> {
     return apiRequest({
       url: `${this.endpoint}/signin`,
       method: "POST",
@@ -14,7 +15,7 @@ export class AuthService extends ApiService {
     });
   }
 
-  async signUp(data: Record<string, unknown>): Promise<unknown> {
+  async signUp(data: Record<string, unknown>): Promise<SuccessApiResponse> {
     return apiRequest({
       url: `${this.endpoint}/signup`,
       method: "POST",
@@ -22,7 +23,7 @@ export class AuthService extends ApiService {
     });
   }
 
-  async signOut(): Promise<unknown> {
+  async signOut(): Promise<SuccessApiResponse> {
     return apiRequest({
       url: `${this.endpoint}/signout`,
       method: "POST",
