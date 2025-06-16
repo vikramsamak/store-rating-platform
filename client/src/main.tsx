b6d089e168d8eb798d54b68f64d2a1051fd8b0b3
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 10:28:39 +0530

    feat: integrate @tanstack/react-query for data fetching

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,18 @@ import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
 import { Toaster } from "@/components/ui/sonner";
 import { AuthProvider } from "./contexts/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={routes} />
-      <Toaster position="top-center" duration={5000} closeButton={true} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={routes} />
+        <Toaster position="top-center" duration={5000} closeButton={true} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
