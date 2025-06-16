9eae8a3a3e2e84f59294f7a88d831b50a30fcaac
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 09:55:14 +0530

    feat: add SystemAdminService for managing system admin operations

--- client/src/services/index.ts
+++ client/src/services/index.ts
@@ -1,5 +1,6 @@
 import { ApiService } from "./apiService";
 import { AuthService } from "./authService";
+import { SystemAdminService } from "./systemAdminService";
 
 export const User = new ApiService("/user");
 
@@ -8,3 +9,5 @@ export const Store = new ApiService("/store");
 export const Rating = new ApiService("/rating");
 
 export const Auth = new AuthService("/auth");
+
+export const SystemAdmin = new SystemAdminService("/systemadmin");
---
+++ client/src/services/systemAdminService.ts
@@ -0,0 +1,19 @@
+import { apiRequest } from "@/utils";
+import { ApiService } from "./apiService";
+import type { SuccessApiResponse } from "@/types";
+
+export class SystemAdminService extends ApiService {
+  constructor(endpoint: string) {
+    super(endpoint);
+  }
+
+  async getOverview(
+    data: Record<string, unknown>
+  ): Promise<SuccessApiResponse> {
+    return apiRequest({
+      url: `${this.endpoint}/overview`,
+      method: "POST",
+      data,
+    });
+  }
+}
