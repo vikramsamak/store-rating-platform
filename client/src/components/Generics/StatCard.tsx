9d8ddff698dac1a712991913b300341faf3a2543
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 19:23:24 +0530

    fix: update StatCard component to handle null values for the value prop

--- client/src/components/Generics/StatCard.tsx
+++ client/src/components/Generics/StatCard.tsx
@@ -1,13 +1,13 @@
 interface StatCardProps {
   label: string;
-  value: string | number;
+  value: string | number | null;
 }
 
 const StatCard: React.FC<StatCardProps> = ({ label, value }) => {
   return (
     <div className="flex flex-col w-full h-full gap-2 bg-muted rounded-md p-2 shadow-md">
       <p className="text-base font-thin">{label}</p>
-      <p className="text-2xl font-bold">{value}</p>
+      <p className="text-2xl font-bold">{value ? value : "NA"}</p>
     </div>
   );
 };
