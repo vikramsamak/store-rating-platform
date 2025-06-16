89a5d59f2ed71fe6f74d1eec3b1967db77c2d18a
vikramsamak <vikramsamak02@gmail.com>
2025-06-16 20:42:24 +0530

    fix: enforce access control for user routes

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -18,31 +18,43 @@ import {
   getUserByIdValidation,
   updateUserValidation,
 } from "../validations/userValidations";
+import { Role } from "../generated/prisma";
 
 export const userRouter = Router();
 
 userRouter.use(authMiddleware);
 
-userRouter.use(accessMiddleware);
-
-userRouter.get("/", validationMiddleware(getUserValidation), getUsers);
+userRouter.get(
+  "/",
+  accessMiddleware([Role.SYSTEM_ADMIN]),
+  validationMiddleware(getUserValidation),
+  getUsers
+);
 
 userRouter.get(
   "/by-id/:id",
+  accessMiddleware([Role.SYSTEM_ADMIN, Role.ADMIN]),
   validationMiddleware(getUserByIdValidation),
   getUserById
 );
 
-userRouter.post("/", validationMiddleware(createUserValidation), createUser);
+userRouter.post(
+  "/",
+  accessMiddleware([Role.SYSTEM_ADMIN, Role.ADMIN]),
+  validationMiddleware(createUserValidation),
+  createUser
+);
 
 userRouter.patch(
   "/:id",
+  accessMiddleware([Role.SYSTEM_ADMIN, Role.ADMIN]),
   validationMiddleware(updateUserValidation),
   updateUser
 );
 
 userRouter.delete(
   "/:id",
+  accessMiddleware([Role.SYSTEM_ADMIN]),
   validationMiddleware(deleteUserValidation),
   deleteUser
 );
